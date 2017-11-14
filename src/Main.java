4345c5b3512a0c1a17398a9ce9c6d4b1732f5a56
bhailat <ma.bhailat@gmail.com>
2017-11-14 12:39:06 +0000

    K-nn with one cross validation optimized

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 import static java.lang.Float.parseFloat;
 import static java.lang.Integer.parseInt;
@@ -25,7 +24,7 @@ public class Main {
         PrintStream print2 = new PrintStream(new FileOutputStream(fileName,true)); //printer in dictionary (add)
 
         ArrayList<ArrayList<String>> outer = new ArrayList<>();
-        ArrayList<String> inner = new ArrayList<String>();
+        ArrayList<String> inner = new ArrayList<>();
 
 
 //read from file and add each sample in arraylist
@@ -49,11 +48,15 @@ public class Main {
            outer.get(i).remove(3);outer.get(i).remove(3);outer.get(i).remove(4);outer.get(i).remove(4);outer.get(i).remove(5);
        }
 
-       //scaling age
+      //scaling age
+/*
 for(int j=1; j<outer.size(); j++){
     float scaleage=(parseFloat(outer.get(j).get(0))-17)/73;
+    //float scaleeduc=(parseFloat(outer.get(j).get(1))-1)/15;
     outer.get(j).set(0,String.valueOf(scaleage));
-}
+   // outer.get(j).set(1,String.valueOf(scaleeduc));
+
+}*/
 
 //male = 1 , female = 0   ( 7353 Male , 3647 Female)
 
@@ -88,61 +91,132 @@ for(int j=1; j<outer.size(); j++){
         float educationS;
         float ageT;
         float educationT;
+        float hoursS;
+        float hoursT;
+        float sexeS;
+        float sexeT;
         float distance=0;
-        float distanceMin=100;
+        float distanceMin=100000;
         int nn=0;
         int count=0;
+        float check;
+        int lessfifty=0; int morefifty=0; String word="";int d;
 
 
-for(int sample=1; sample<outer.size(); sample++){
-  //  System.out.println(outer.get(sample).get(6)+ "sample");
-    System.out.println("--------------------"+sample+"------------------------------------------------------------------");
+        ArrayList<ArrayList<Float>> samplem = new ArrayList<>();
+        ArrayList<Float> distancem = new ArrayList<>();
+        ArrayList<Float> distancecheck=new ArrayList<>();
+        distancecheck.add((float) 10);
 
-    if(outer.get(sample).get(6).contentEquals("5")){
 
-        ageS=parseFloat(outer.get(sample).get(0));
-        educationS=parseFloat(outer.get(sample).get(1));
 
-        for(int test=1; test<outer.size(); test++){
-            //System.out.println(outer.get(test).get(6) + " fold");
-            if(outer.get(test).get(6)!=outer.get(2).get(6)){
-             ageT=parseFloat(outer.get(test).get(0));educationT=parseFloat(outer.get(test).get(1));
-            // System.out.println("ageS "+ageS+ " agetT "+ageT+" sexeS "+sexeS+" sexeT "+sexeT );
-            distance= (float) Math.pow(ageS-ageT,2);
-             //System.out.println(distance+ " distance age");
+        for(int sample=1; sample<outer.size(); sample++){
+            System.out.println("sample n°"+sample);
 
-             distance=distance+(float)Math.pow(educationS-educationT,2);
 
-             //System.out.println(distance + " square");
-             distance= (float) Math.sqrt(distance);
-            // System.out.println(distance + " of sample "+ outer.indexOf(outer.get(test)));
+
+            if(outer.get(sample).get(6).contentEquals("5")){
+
+                samplem.clear();
+                distancem.clear();
+                distancecheck.clear();
+                distancecheck.add((float) 10);
+
+                lessfifty=0;
+                morefifty=0;
+                word="";
+                ageS=parseFloat(outer.get(sample).get(0)); educationS=parseFloat(outer.get(sample).get(1)); hoursS=parseFloat(outer.get(sample).get(4)); sexeS=parseFloat(outer.get(sample).get(3));
+
+
+                for(int test=1; test<outer.size(); test++){
+
+                    if(parseInt(outer.get(test).get(6))!=5){
+
+                        ageT=parseFloat(outer.get(test).get(0)); educationT=parseFloat(outer.get(test).get(1)); hoursT=parseFloat(outer.get(test).get(4)); sexeT=parseFloat(outer.get(test).get(3));
+                        distance= (float) Math.pow(ageT-ageS,2)+(float)Math.pow(educationT-educationS,2)+(float)Math.pow(hoursT-hoursS,2)+(float)Math.pow(sexeT-sexeS,2);
+                        distance= (float) Math.sqrt(distance);
+
+                        if(distancecheck.size()<35){
+                            distancecheck.add(distance);
+                            Collections.sort(distancecheck);
+                            distancem.add(distance);
+                            distancem.add((float) outer.indexOf(outer.get(test)));
+                            samplem.add(distancem);
+                        }
+                        if(distancecheck.size()==35){
+                            if(distance<distancecheck.get(34)){
+                                distancecheck.remove(34);
+                                distancecheck.add(distance);
+                                distancem.add(distance);
+                                distancem.add((float) outer.indexOf(outer.get(test)));
+                                samplem.add(distancem);
+                            }
+                            Collections.sort(distancecheck);
+
+                        }
+
+                       // distancecheck.add(distance);
+
+
+                        //distancem.add(distance);
+                        //distancem.add((float) outer.indexOf(outer.get(test)));
+                       // samplem.add(distancem);
+                    }
+                }
+
+                // get K
+               // System.out.println(distancecheck);
+                //Collections.sort(distancecheck);
+                check=distancecheck.get(34);
+                System.out.println("distancecheck "+check);
+
+                //remove all element if distance is larger then K
+                for(int i=0; i<samplem.size();i++){
+                    if(samplem.get(i).get(0)<=check){
+
+                        float c= samplem.get(i).get(1);
+                        d= Math.round(c);
+
+                        if(outer.get(d).get(5).contentEquals("<=50K")){
+                            lessfifty++;
+                        }
+
+                        if(outer.get(d).get(5).contentEquals(">50K")){
+                            morefifty++;
+                        }
+
+                        //samplem.remove(i);
+                       // i--;
+                    }
+
+
+
+                }
+
+
+                if(morefifty>lessfifty)  {
+                    word=">50K";
+                }
+                else {
+                    word="<=50K";
+                }
+
+                if(outer.get(sample).get(5).contentEquals(word)){
+                    count++;
+                    System.out.println(count + "count");
+                }
             }
-            if(distance<distanceMin){ distanceMin=distance;nn=outer.indexOf(outer.get(test)); }
 
         }
-       // System.out.println(distanceMin + "distance min");
 
-    }
-    if(outer.get(sample).get(5).contentEquals(outer.get(nn).get(5))){
-        System.out.println(outer.get(sample).get(5)+ " sample n"+ outer.indexOf(outer.get(sample))+ "earn");
-        System.out.println(outer.get(nn).get(5)+ " sample n"+ outer.indexOf(outer.get(nn))+ "earn");
-
-        count++;
-
-    }
+        System.out.println(count + "count");
 
 
-distanceMin=100;
 
 }
-System.out.println(count + "count");
-
-
-
-
-
 
 
     }
-}
+
+
 
